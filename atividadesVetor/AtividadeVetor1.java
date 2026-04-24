dba91fa62f99be1e00c4d13a9c6c52fbc4401856
GimendescCodes <giovannamendessenai@gmail.com>
2026-04-24 14:46:22 -0300

    atividadeVetor1

---
+++ atividadesVetor/AtividadeVetor1.java
@@ -0,0 +1,36 @@
+package atividadesVetor;
+
+import java.util.Scanner;
+
+public class AtividadeVetor1 {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
+		//variaveis
+		int vetorNumeros[] = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+		int numero;
+		boolean encontrado = false; //Variável de controle para saber se o número foi encontrado ou não
+
+		//entrada do numero escolhido pelo úsuario
+		System.out.println("Digite o número que você deseja encontrar: ");
+		numero = leia.nextInt();
+		
+		//Laço de repetição que percorre todas as posições do vetor
+		for (int posicao = 0; posicao < vetorNumeros.length; posicao++) {
+
+			//identifica se o numero escolhido pelo usuario é uma das opções e ja responde sua posição
+			if (vetorNumeros[posicao] == numero) {
+				System.out.println("O número " + numero + " está na posição " + posicao);
+				encontrado = true;
+			}
+		}
+
+		//identifica que numúmero escolhido não era uma das opções
+		if (!encontrado) {
+			System.out.println("O número "+ numero + " não foi encontrado!");
+		}
+	}
+
+}
